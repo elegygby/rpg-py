4f66aa0bb70f542d4ae84176363a7f5e56cdd1e1
Gregory <gbublitzbarcaro@gmail.com>
2024-05-29 14:00:20 -0300

    aterações gerais

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import time
 import os 
 
-#função de limpar o terminal
-def clear_screan():
-    os.system('cls')
+# Função de limpar o terminal
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-#heroi principal
+list_monster = []
+
+# Herói principal
 link = {
     'nome': 'Link',
     'level': 1,
@@ -16,21 +18,30 @@ link = {
     'atk': 3
 }
 
-#atual inimigo principal
-monster = {
-    'nome': 'Goblin',
-    'HP': 2,
-    'atk': 2,
-    'exp': 5
-}
+# Função para criar inimigos
+def create_monster(level):
+    new_monster = {
+        'nome': f"Goblin #{level}",
+        'HP': 2 + level,
+        'atk': 2 + level,
+        'level': level,
+        'exp': 5 * level
+    }
+    return new_monster
 
-#função para ganhar exp e upar
-def give_experience():
+# Função para gerar lista de monstros
+def gen_monster(n_monster):
+    for x in range(n_monster):
+        monster = create_monster(x + 1)
+        list_monster.append(monster)
+
+# Função para ganhar exp e upar
+def give_experience(monster):
     link['exp'] += monster['exp']
     print(f"{link['nome']} ganhou {monster['exp']} pontos de experiencia!")
     up_level()
 
-#função para upar
+# Função para upar
 def up_level():
     if link['exp'] >= link['exp_max']:
         link['level'] += 1
@@ -40,73 +51,79 @@ def up_level():
         link['atk'] += 2
         print(f"{link['nome']} upou de level!")
 
-#função de atacar inimigo
-def atack_npc():
-    monster['HP'] = monster['HP'] - link['atk']
+# Função de atacar inimigo
+def atack_npc(monster):
+    monster['HP'] -= link['atk']
+    print(f"{link['nome']} atacou {monster['nome']} e causou {link['atk']} de dano.")
 
-#função de ser atacado
-def atack_link():
+# Função de ser atacado
+def atack_link(monster):
     link['HP'] -= monster['atk']
-#função de luta
-def fight():
-    atack_npc()
-    atack_link()
+    print(f"{monster['nome']} atacou {link['nome']} e causou {monster['atk']} de dano.")
 
-#função de morte de npc
-def death_npc():
+# Função de morte de npc
+def death_npc(monster):
     if monster['HP'] <= 0:
-        print('você venceu!!')
-        give_experience()
+        print(f"{monster['nome']} foi derrotado!")
+        give_experience(monster)
         return True
     return False
 
-#função para game over
+# Função para game over
 def death_link():
     if link['HP'] <= 0:
         print('GAME OVER!!')
         return True
     return False
 
-#função para mostrar heroi
+# Função para mostrar herói
 def show_link():
-    print(
-        f"Nome: {link['nome']} // Vida: {link['HP']} // Level: {link['level']} // Experiencia: {link['exp']}"
-    )
+    print(f"Nome: {link['nome']} // Vida: {link['HP']} // Level: {link['level']} // Experiencia: {link['exp']}")
 
-def show_monster():
-    print(
-        f"Nome: {monster['nome']} // Vida: {monster['HP']}// Ataque: {monster['atk']} // Experiencia: {monster['exp']}"
-   )
+# Função para mostrar monstro
+def show_monster(monster):
+    print(f"Nome: {monster['nome']} // Vida: {monster['HP']} // Ataque: {monster['atk']} // Experiencia: {monster['exp']}")
 
-#fluxo principal do jogo
+# Fluxo principal do jogo
+gen_monster(5)  # Gerar 5 monstros
+
 while True:
-    clear_screan()
+    clear_screen()
     print('--------------')
     show_link()
-    show_monster()
+    if list_monster:
+        current_monster = list_monster[0]
+        show_monster(current_monster)
+    else:
+        print('Não há mais monstros.')
+        break
     print('--------------')
 
-    action = input('O que quer fazer?  (1)atacar inimigo  //  (2)Sair ')
+    action = input('O que quer fazer?  (1) Atacar inimigo  //  (2) Sair ')
 
     if action == '1':
-        atack_npc()
-        if death_npc():
-            break
-        atack_link()
-        if death_link():
-            break
+        atack_npc(current_monster)
+        if death_npc(current_monster):
+            list_monster.pop(0)
+            if not list_monster:
+                print('Todos os monstros foram derrotados! Você venceu!')
+                break
+        else:
+            atack_link(current_monster)
+            if death_link():
+                break
     elif action == '2':
         break
     else:
         print('AÇÃO INVALIDA')
-    
+
 show_link()
 
 time.sleep(8)
 
-clear_screan()
+clear_screen()
 
 print('Jogo Terminado')
 
 time.sleep(5)
-clear_screan()
+clear_screen()
